e97bf9b08477790181040d30bb553a6b982b40ed
John Whiles <johnwhiles@outlook.com>
2016-11-07 14:25:42 +0000

    enter the matrix

--- server.js
+++ server.js
@@ -1,5 +1,15 @@
 var http = require('http');
-var server = http.createServer();
+var message = 'I am complacent about being in a girl group';
+
+
+function handler(request, response){
+  response.writeHead(200, {"content-type":"text/html"});
+  response.write(message);
+  response.end();
+};
+
+
+var server = http.createServer(handler);
 
 server.listen(3000,function(){
   console.log("Server is listening on port 3000.  Ready to accept requests!");
