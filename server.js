09dea51eee624529f1133c083fa41fbd456b9d09
John Whiles <johnwhiles@outlook.com>
2016-11-07 15:53:26 +0000

    remove all our work

--- server.js
+++
@@ -1,9 +0,0 @@
-var http = require('http');
-var handler = require('./src/handler.js')
-
-var server = http.createServer(handler);
-console.log("Server directory name: ",__dirname);
-
-server.listen(3000,function(){
-  console.log("Server is listening on port 3000.  Ready to accept requests!");
-});
