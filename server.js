77f28e0450b49edcc2493cf68498fd4493da3ed2
John Whiles <johnwhiles@outlook.com>
2016-11-07 15:18:10 +0000

    add a horse image, also form submissions

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 var http = require('http');
-var fs = require('fs')
+var fs = require('fs');
+var querystring = require('querystring');
+var path = require('path');
+
+
+// mesages
 var message = 'I am complacent about being in a girl group';
 var nodeMessage = 'I am a little node, I have a little toad';
-var girlsMessage = 'teapot teapot teapot teapot teapoteteapot teapot teapot teapot teapoteteapot teapot teapot teapot teapoteteapot teapot teapot teapot teapoteteapot teapot teapot teapot teapoteteapot teapot teapot teapot teapoteteapot teapot teapot teapot teapoteteapot teapot teapot teapot teapoteteapot teapot teapot teapot teapoteteapot teapot teapot teapot teapote'
-var path = require('path');
+var girlsMessage = 'teapot teapot teapot teapot';
+
+
+
+
+
 function handler(request, response){
   var endpoint = request.url;
   var extension = path.extname(endpoint).substring(1);
@@ -24,6 +33,18 @@ function handler(request, response){
   } else if (endpoint === '/girls') {
     response.writeHead(200, { "content-type": "text/html" });
     response.end(girlsMessage);
+  } else if(endpoint === "/create-post"){
+    response.writeHead(303, { "Location": "/index.html" });
+    var allTheData = "";
+    request.on('data', function(chunkOfData){
+      allTheData += chunkOfData;
+    });
+    request.on('end', function(){
+      var convertedData = querystring.parse(allTheData);
+      console.log(convertedData);
+      response.end();
+    })
+
   } else {
     response.writeHead(200, {"content-type":"text/" + extension});
     fs.readFile(__dirname + '/public'+endpoint, function(err,file){
